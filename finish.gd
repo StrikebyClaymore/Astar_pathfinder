e2ede5710011a3d7f50543ae6e94443574012f08
StrikebyClaymore <36325557+StrikebyClaymore@users.noreply.github.com>
2018-10-15 02:41:21 +0300

    pathfinder

--- finish.gd
+++ finish.gd
@@ -1,11 +1,14 @@
 extends Area2D
 
+var win = false
+
 func _ready():
 	pass
 
 func _on_finish_body_entered(body):
-	if body.is_in_group("mob"):
+	if body.is_in_group("mob") && !win:
 		var l = Label.new()
 		l.rect_position = Vector2(960/2, 640/2)
 		l.set_text("YOU WIN")
 		get_parent().add_child(l)
+		win = true
